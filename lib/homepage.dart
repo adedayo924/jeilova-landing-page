9c1595dd3505090385498b1838b118a626bbddeb
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2023-06-07 10:19:55 +0100

    Updating the landing page UI and other files

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,68 +11,166 @@ class HomePage extends StatelessWidget {
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
           colors: [
-            Colors.green,
-            Colors.lightGreenAccent,
-            Colors.lightGreen,
+            Color.fromARGB(255, 13, 161, 18),
+            Color.fromARGB(255, 59, 100, 12),
+            Color.fromARGB(255, 71, 133, 2),
           ],
         ),
       ),
       child: const Scaffold(
         backgroundColor: Colors.transparent,
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 100,
-            vertical: 40,
-          ),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 100,
+              vertical: 40,
+            ),
+            child: Column(
               children: [
-                Text(
-                  'Jeilova',
-                  style: TextStyle(
-                    letterSpacing: 1,
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'Home',
+                      'Jeilova',
                       style: TextStyle(
+                        letterSpacing: 1,
                         color: Colors.white,
-                        fontSize: 20,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(width: 40),
-                    Text(
-                      'Features',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
+                    Row(
+                      children: [
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Text(
+                          'Features',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 183, 255, 187),
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Text(
+                          'About',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 183, 255, 187),
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 40),
-                    Text(
-                      'About',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 100),
+                        Text(
+                          'AI Virtual Assistant!',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 183, 255, 187),
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Jeilova',
+                          style: TextStyle(
+                            letterSpacing: 5,
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: 500,
+                          child: Text(
+                            'This is an AI virtual Assistant App build on Flutter using Dart programming language. This app is still under development and will be available soon on Google Play Store. For testing purpose, you can download the app from the link below.',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 183, 255, 187),
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        SizedBox(
+                          width: 200,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image(
+                                image: AssetImage('images/openai.png'),
+                                width: 70,
+                                color: Color.fromARGB(255, 183, 255, 187),
+                              ),
+                              Image(
+                                image: AssetImage('images/gimp.png'),
+                                width: 30,
+                                color: Color.fromARGB(255, 183, 255, 187),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          height: 50,
+                          width: 200,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(
+                                Icons.download,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                'Download',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(width: 40),
-                    Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: 40,
+                    Image(
+                      image: AssetImage('images/virtual-assistant.png'),
+                      width: 450,
                     ),
                   ],
                 ),
               ],
-            )
-          ]),
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Jeilova Landing Page',
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.green,
       ),
       home: const HomePage(),
